7e86416d487d1aa6a98d5d2c1a6a3efe3ee5ca51
« Achille <achille@deribreux.be>
2022-10-15 20:25:32 +0200

    feat(word)/ add custom repo

--- src/main/java/com/woquiz/quiz/dto/WordCriteria.java
+++ src/main/java/com/woquiz/quiz/dto/WordCriteria.java
@@ -1,4 +1,107 @@
 package com.woquiz.quiz.dto;
 
+import java.util.List;
+
+import com.woquiz.quiz.model.Word;
+
 public class WordCriteria {
+
+    private String basicWordContains;
+    private String translationContains;
+    private List<Word.WordStatus> status;
+    private List<Word.WordLevel> level;
+    private Integer userId;
+    private Integer pageNr;
+    private Integer pageSize;
+
+    public String getBasicWordContains() {
+        return basicWordContains;
+    }
+
+    public void setBasicWordContains(String basicWordContains) {
+        this.basicWordContains = basicWordContains;
+    }
+
+    public WordCriteria basicWordContains(String basicWordContains){
+        setBasicWordContains(basicWordContains);
+        return this;
+    }
+
+    public String getTranslationContains() {
+        return translationContains;
+    }
+
+    public void setTranslationContains(String translationContains) {
+        this.translationContains = translationContains;
+    }
+
+    public WordCriteria translationContains(String translationContains){
+        setTranslationContains(translationContains);
+        return this;
+    }
+
+    public List<Word.WordStatus> getStatus() {
+        return status;
+    }
+
+    public void setStatus(List<Word.WordStatus> status) {
+        this.status = status;
+    }
+
+    public WordCriteria status(List<Word.WordStatus> status){
+        setStatus(status);
+        return this;
+    }
+
+    public List<Word.WordLevel> getLevel() {
+        return level;
+    }
+
+    public void setLevel(List<Word.WordLevel> level) {
+        this.level = level;
+    }
+
+    public WordCriteria level(List<Word.WordLevel> level){
+        setLevel(level);
+        return this;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public WordCriteria userId(Integer userId){
+        setUserId(userId);
+        return this;
+    }
+
+    public Integer getPageNr() {
+        return pageNr;
+    }
+
+    public void setPageNr(Integer pageNr) {
+        this.pageNr = pageNr;
+    }
+
+    public WordCriteria pageNr(Integer pageNr){
+        setPageNr(pageNr);
+        return this;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public WordCriteria pageSize(Integer pageSize){
+        setPageSize(pageSize);
+        return this;
+    }
 }
--- src/main/java/com/woquiz/quiz/repository/CustomWordRepositoryImpl.java
+++ src/main/java/com/woquiz/quiz/repository/CustomWordRepositoryImpl.java
@@ -1,13 +1,50 @@
 package com.woquiz.quiz.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import com.woquiz.common.utils.BasicUtils;
 import com.woquiz.quiz.dto.WordCriteria;
 import com.woquiz.quiz.model.Word;
 
 public class CustomWordRepositoryImpl implements CustomWordRepository{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
     @Override
     public List<Word> findByCriteria(WordCriteria wordCriteria) {
-        return null;
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Word> criteriaQuery = criteriaBuilder.createQuery(Word.class);
+        BasicUtils.ConditionalList<Predicate> conditionalList = BasicUtils.ConditionalList.of(new ArrayList<>());
+        Root<Word> wordRoot = criteriaQuery.from(Word.class);
+        conditionalList
+                .add(wordCriteria.getBasicWordContains()!=null,()->criteriaBuilder.like((criteriaBuilder.upper(wordRoot.get("BASIC_WORD"))),toLike(wordCriteria.getBasicWordContains())))
+                .add(wordCriteria.getTranslationContains()!=null,()->criteriaBuilder.like((criteriaBuilder.upper(wordRoot.get("TRANSLATION"))),toLike(wordCriteria.getTranslationContains())))
+                .add(wordCriteria.getUserId()!=null,()->criteriaBuilder.equal(wordRoot.get("USER_ID"),wordCriteria.getUserId()))
+                .add(wordCriteria.getStatus()!=null,()-> wordRoot.get("STATUS").in(wordCriteria.getStatus()))
+                .add(wordCriteria.getLevel()!=null,()-> wordRoot.get("LEVEL").in(wordCriteria.getLevel()));
+        criteriaQuery.where(conditionalList.toList().toArray(new Predicate[0]));
+        criteriaQuery.orderBy(criteriaBuilder.desc(wordRoot.get("ID")));
+        TypedQuery<Word> query = entityManager.createQuery(criteriaQuery);
+
+        if(wordCriteria.getPageSize()!=null && wordCriteria.getPageNr()!=null)
+            query.setMaxResults(wordCriteria.getPageSize()).setFirstResult((wordCriteria.getPageNr()-1) * wordCriteria.getPageSize());
+        return query.getResultList();
+    }
+
+    private String toLike(String value){
+        if(value.contains("*"))
+            return value.replace("*","%");
+        return "%"+value+"%";
     }
 }
